d08a73c7f3b9979c075d885700cccce771175a96
Manh Cuong <cuong@radioactive.sg>
2023-05-29 17:49:27 +0800

    update contract info

--- app/Http/Controllers/InvitationLetterController.php
+++ app/Http/Controllers/InvitationLetterController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Models\IndividualCustomer;
+use App\Http\Models\Customer;
 use App\Http\Models\InvitationLetter;
+use App\Admin\Controllers\Constant;
 
 class InvitationLetterController extends Controller
 {
@@ -12,7 +13,11 @@ class InvitationLetterController extends Controller
     public function find(Request $request)
     {
         $id = $request->get('q');
-        return InvitationLetter::find($id);
+        $invitationLetter = InvitationLetter::find($id);
+        $invitationLetter->customer_type = Constant::CUSTOMER_TYPE[$invitationLetter->customer_type];
+        $invitationLetter->payment_method = is_null($invitationLetter->payment_method) ? null : Constant::PAYMENT_METHOD[$invitationLetter->payment_method];
+        $invitationLetter->vat = is_null($invitationLetter->vat) ? null : Constant::YES_NO[$invitationLetter->vat];
+        return $invitationLetter;
     }
     
 }
