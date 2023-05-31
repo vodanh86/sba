78c3fad510c4495b3a08773d925648385188bc50
Manh Cuong <cuong@radioactive.sg>
2023-05-31 14:52:51 +0800

    add pre assessment comment

--- app/Admin/Controllers/PreAssessmentController.php
+++ app/Admin/Controllers/PreAssessmentController.php
@@ -6,7 +6,7 @@ use App\Http\Models\PreAssessment;
 use Encore\Admin\Controllers\AdminController;
 use App\Http\Models\Contract;
 use App\Http\Models\AdminUser;
-use App\Http\Models\Status;
+use App\Admin\Actions\Document\AddPreAssessmentComment;
 use App\Http\Models\StatusTransition;
 use Encore\Admin\Facades\Admin;
 use Encore\Admin\Form;
@@ -50,8 +50,9 @@ class PreAssessmentController extends AdminController
             return $this->statusDetail->name;
         });
 
-        $grid->column('created_at', __('Created at'));
-        $grid->column('updated_at', __('Updated at'));
+        $grid->column('comment')->action(AddPreAssessmentComment::class)->width(150);
+        $grid->column('created_at', __('Created at'))->width(150);
+        $grid->column('updated_at', __('Updated at'))->width(150);
 
         $grid->model()->where('branch_id', '=', Admin::user()->branch_id)->whereIn('status', array_merge($viewStatus, $editStatus, $approveStatus));
         if (Utils::getCreateRole(Constant::PRE_ASSESS_TABLE) != Admin::user()->roles[0]->slug){
