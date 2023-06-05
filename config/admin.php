298988ca90a10f5db9be88d52de997c20de5fc2a
Manh Cuong <cuong@radioactive.sg>
2023-06-05 15:37:12 +0800

    update favicon

--- config/admin.php
+++ config/admin.php
@@ -249,7 +249,7 @@ return [
     | Set a default avatar for newly created users.
     |
     */
-    'default_avatar' => '/vendor/laravel-admin/AdminLTE/dist/img/user2-160x160.jpg',
+    'default_avatar' => 'https://sba.net.vn/wp-content/uploads//2020/09/LOGO-SBA-SVG-01.svg',
 
     /*
     |--------------------------------------------------------------------------
