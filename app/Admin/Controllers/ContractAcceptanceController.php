882834f2608d51dcfc820e47abd5fbc392d74355
cuongdm <>
2023-08-01 11:53:42 +0700

    change unique contract accept

--- app/Admin/Controllers/ContractAcceptanceController.php
+++ app/Admin/Controllers/ContractAcceptanceController.php
@@ -214,7 +214,7 @@ class ContractAcceptanceController extends AdminController
         }
         $form->select('contract_id', __('valuation_document.contract_id'))->options(Contract::where("branch_id", Admin::user()->branch_id)->where('status', Constant::CONTRACT_INPUTTING_STATUS)->whereHas('scoreCards', function ($query) {
             $query->where('status', 74);
-        })->pluck('code', 'id'))->required();
+        })->pluck('code', 'id'))->required()->creationRules('unique:contract_acceptances');
         $form->textarea('property', __('Tài sản thẩm định giá'))->disable();
         $form->date('date_acceptance', __('Ngày nghiệm thu'));
 
