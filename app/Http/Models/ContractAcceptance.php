a0d0b801262f794868ca2e03cf00333d3b400724
VanThuan76 <thuanvuvan76@gmail.com>
2023-08-01 13:47:43 +0700

    update_1_8_p3

--- app/Http/Models/ContractAcceptance.php
+++ app/Http/Models/ContractAcceptance.php
@@ -8,21 +8,26 @@ class ContractAcceptance extends Model
 {
     protected $table = 'contract_acceptances';
 
-	protected $hidden = [
-    ];
-
-	protected $guarded = [];
 
     public function contract()
     {
         return $this->belongsTo(Contract::class, 'contract_id');
+    }
+    public function assistant()
+    {
+        return $this->belongsTo(AdminUser::class, 'tdv_assistant');
+    }
+
+    public function supervisorDetail()
+    {
+        return $this->belongsTo(AdminUser::class, 'supervisor');
     }
 
     public function statusDetail()
     {
         return $this->belongsTo(Status::class, 'status');
     }
-    
+
     public function setDocumentAttribute($documents)
     {
         if (is_array($documents)) {
@@ -34,4 +39,7 @@ class ContractAcceptance extends Model
     {
         return is_null($documents) ? [] : json_decode($documents, true);
     }
+    protected $hidden = [];
+
+    protected $guarded = [];
 }
