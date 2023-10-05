e01596b1f561461e89d972046172fb6b040486ec
DESKTOP-FJ9BBRK\Admin <cuongdm172@gmail.com>
2023-10-05 17:03:58 +0700

    fix enddate

--- app/Admin/Forms/SupervisorReport.php
+++ app/Admin/Forms/SupervisorReport.php
@@ -30,7 +30,7 @@ class SupervisorReport extends Form
         $result = array("from_date" => $request->get("from_date"),
                         "to_date" => $request->get("to_date"),
                         "formated_from_date" => $fromDate->format('Y-m-d'),
-                        "formated_to_date" => $toDate->format('Y-m-d'));
+                        "formated_to_date" => $toDate->format('Y-m-d 23:59:59'));
         return back()->with(['result' => $result]);
     }
 
