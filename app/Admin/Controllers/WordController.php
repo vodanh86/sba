2f1accb841217c6f144ee6675d7187dd655f753f
DESKTOP-FJ9BBRK\Admin <cuongdm172@gmail.com>
2023-10-06 11:37:24 +0700

    add word controller

---
+++ app/Admin/Controllers/WordController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use Encore\Admin\Controllers\AdminController;
+use Illuminate\Http\Request;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+use App\Http\Models\Contract;
+
+class WordController extends AdminController
+{
+    public function createContract(Request $request)
+    {
+        $id = $request->input('id');
+        $contract = Contract::find($id);
+        $document = new \PhpOffice\PhpWord\TemplateProcessor(public_path()."/template/SBA-HDDN.docx");
+        $document->setValue('address',  $contract->business_address);
+        $document->setValue('taxNumber',  $contract->tax_number);
+        $document->saveAs(storage_path()."output.docx");
+
+        return response()->file(storage_path()."output.docx");
+    }
+}
