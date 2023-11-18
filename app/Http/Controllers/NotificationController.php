6ec7cc0fd1f17c675cc7d5216ae6871bdd8f8f9e
ThuanHipp <88172491+VanThuan76@users.noreply.github.com>
2023-11-18 14:51:13 +0700

    thuanvv/fix-issue-notify-v2

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Models\AdminUser;
 use App\Http\Models\Notification;
+use App\Http\Models\NotifyStatus;
 use Pusher\Pusher;
 use App\Admin\Controllers\Constant;
 
@@ -15,26 +16,37 @@ class NotificationController extends Controller
             $adminUser = AdminUser::find($userId);
             return $adminUser ? $adminUser->name : '';
         };
-        $notifications = Notification::where('status', 0)->orderBy('id', 'desc')->get();
-        foreach ($notifications as $notification) {
-            $options = array(
-                'cluster' => 'ap1',
-                'encrypted' => true
-            );
-            $pusher = new Pusher(
-                env('PUSHER_APP_KEY'),
-                env('PUSHER_APP_SECRET'),
-                env('PUSHER_APP_ID'),
-                $options
-            );
-            $notification['status'] = 1;
-            $notification->save();
+        $notifyStatus = NotifyStatus::where('id', 1)->first();
+        if ($notifyStatus->status == 1) {
+            return response()->json(200);
+        } else {
+            $notifyStatus->status == 1;
+            $notifyStatus->save();
 
-            $notification['user_send'] = $convertIdToNameUser($notification->user_send);
-            $pusher->trigger(Constant::PUSHER_CHANNEL, Constant::PUSHER_EVENT, $notification);
-            usleep(500000);
+            $notifications = Notification::where('status', 0)->limit(10)->get();
+            foreach ($notifications as $notification) {
+                $options = array(
+                    'cluster' => 'ap1',
+                    'encrypted' => true
+                );
+                $pusher = new Pusher(
+                    env('PUSHER_APP_KEY'),
+                    env('PUSHER_APP_SECRET'),
+                    env('PUSHER_APP_ID'),
+                    $options
+                );
+                $notification['status'] = 1;
+                $notification->save();
+
+                $notification['user_send'] = $convertIdToNameUser($notification->user_send);
+                $pusher->trigger(Constant::PUSHER_CHANNEL, Constant::PUSHER_EVENT, $notification);
+                usleep(500000);
+            }
+            
+            $notifyStatus->status == 0;
+            $notifyStatus->save();
+            return response()->json(200);
         }
-        return response()->json(200);
     }
 
     public function check($id)
---
+++ app/Http/Models/NotifyStatus.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class NotifyStatus extends Model
+{
+    protected $table = 'notify_status';
+
+	protected $hidden = [
+    ];
+
+	protected $guarded = [];
+}
